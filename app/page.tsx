6c745305cefd242e9b465339b62562a6134f5bc5
RahulKathayat <rkathayat1103@gmail.com>
2023-09-19 00:02:59 +0530

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <p className="text-3xl text-indigo-500">hello jee, kaise ho sare ?</p>
+    <p className="text-3xl text-indigo-500">hello jee, kaise ho sare ? <Button>hello button</Button></p>
   )
 }
