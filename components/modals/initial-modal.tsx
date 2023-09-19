fed49ba8b93a6389e6a35be76b158400d2b21904
RahulKathayat <rkathayat1103@gmail.com>
2023-09-19 23:30:44 +0530

    init modal finished

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -22,6 +22,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { FileUpload } from "@/components/file-upload";
 import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
@@ -75,22 +76,21 @@ export const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                {/* <FormField
+                <FormField
                   control={form.control}
                   name="imageUrl"
                   render={({ field }) => (
                     <FormItem>
                       <FormControl>
                         <FileUpload
-                          endpoint="serverImage"
-                          value={field.value}
-                          onChange={field.onChange}
+                        endpoint="serverImage"
+                        value={field.value}
+                        onChange={field.onChange}
                         />
                       </FormControl>
                     </FormItem>
                   )}
-                /> */}
-                TODO: Image Upload
+                />
               </div>
 
               <FormField
