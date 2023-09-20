28b35d6d671de407734647e0811d0e614b7deccf
RahulKathayat <rkathayat1103@gmail.com>
2023-09-21 00:39:53 +0530

    big commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ModalProvider } from '@/components/providers/modal-provider'
+
 import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
@@ -27,6 +29,7 @@ export default function RootLayout({
           enableSystem
           storageKey='drocsid-app'
           >
+            <ModalProvider/>
             {children}
           </ThemeProvider>
         </body>
